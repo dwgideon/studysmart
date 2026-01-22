2c178ba15a46debc0f8fd5e8d1e0717a26420c2b
David Gideon <davidgideon@Mac.hsd1.ga.comcast.net>
2026-01-22 14:43:02 -0500

    Convert useStudyStore to TypeScript for Vercel build

--- src/store/useStudyStore.ts
+++ src/store/useStudyStore.ts
@@ -1,21 +1,40 @@
 import { create } from "zustand";
 
-export const useStudyStore = create((set) => ({
+export type StudyFile = {
+  id: number;
+  name?: string;
+  active?: boolean;
+  [key: string]: any;
+};
+
+type StudyStore = {
+  files: StudyFile[];
+  addFile: (file: Partial<StudyFile>) => void;
+  deleteFile: (id: number) => void;
+  renameFile: (id: number, newName: string) => void;
+  setActiveFile: (id: number) => void;
+};
+
+export const useStudyStore = create<StudyStore>((set) => ({
   files: [],
+
   addFile: (file) =>
     set((state) => ({
       files: [...state.files, { id: Date.now(), ...file }],
     })),
+
   deleteFile: (id) =>
     set((state) => ({
       files: state.files.filter((file) => file.id !== id),
     })),
+
   renameFile: (id, newName) =>
     set((state) => ({
       files: state.files.map((file) =>
         file.id === id ? { ...file, name: newName } : file
       ),
     })),
+
   setActiveFile: (id) =>
     set((state) => ({
       files: state.files.map((file) => ({
@@ -24,4 +43,3 @@ export const useStudyStore = create((set) => ({
       })),
     })),
 }));
-
